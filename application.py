bd87d609d984a0ac59866c89bf86662384883023
tukavic <thomasla1@outlook.com>
2021-07-18 21:58:08 -0700

    update to application.py

--- application.py
+++ application.py
@@ -5,28 +5,28 @@ import os
 
 from flask import Flask, render_template, request, redirect, Response, send_file, abort
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 try:
-    app.config['GA_TRACKING_ID'] = os.environ['GA_TRACKING_ID']
+    application.config['GA_TRACKING_ID'] = os.environ['GA_TRACKING_ID']
 except:
     print('Tracking ID not set')
 
 
-@app.route('/')
+@application.route('/')
 def index():
     age = int((datetime.date.today() - datetime.date(1992, 3, 4)).days / 365)
     return render_template('home.html', age=age)
 
 
-@app.route('/projects')
+@application.route('/projects')
 def projects():
     data = get_static_json("static/projects/luke_projects.json")['projects']
     data.sort(key=order_projects_by_weight, reverse=True)
     return render_template('projects.html',  projects=data)
 
 
-@app.route('/acknowledgements')
+@application.route('/acknowledgements')
 def acknowledgements():
     data = get_static_json(
         "static/projects/acknowledgements.json")['acknowledgements']
@@ -34,7 +34,7 @@ def acknowledgements():
     return render_template('acknowledgements.html',  acknowledgements=data)
 
 
-@app.route('/blog')
+@application.route('/blog')
 def blog():
     return redirect("http://www.aPaperADay.com/", code=302)
 
@@ -46,14 +46,14 @@ def order_projects_by_weight(projects):
         return 0
 
 
-@app.route('/resume')
+@application.route('/resume')
 def resume():
     data = {"name": "Luke Thomas Résumé",
             "pdf_link": "/static/pdfs/resume.pdf"}
     return render_template('pdf.html', project=data)
 
 
-@app.route('/publish/<pdf>')
+@application.route('/publish/<pdf>')
 def pdf(pdf):
     data = get_static_json("static/projects/luke_projects.json")['projects']
     current = None
@@ -68,7 +68,7 @@ def pdf(pdf):
         return render_template('pdf.html', project=current)
 
 
-@app.errorhandler(404)
+@application.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
@@ -84,4 +84,4 @@ def get_static_json(path):
 
 if __name__ == "__main__":
     print("running py app")
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    application.run(host="127.0.0.1", port=5000, debug=True)
