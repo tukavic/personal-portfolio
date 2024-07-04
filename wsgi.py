f279e18e8ae2349981d5022feb8f4b84f28b4654
tukavic <39752064+tukavic@users.noreply.github.com>
2024-07-04 13:41:35 -0700

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,1 +1,16 @@
+# wsgi.py
+
+import os
+import sys
+import site
+
+# Add the site-packages of the virtualenv
+site.addsitedir(os.path.join(os.path.dirname(__file__), 'venv', 'lib', 'python3.8', 'site-packages'))
+
+# Add the app directory to the sys.path
+sys.path.insert(0, os.path.dirname(__file__))
+
+# Set the application object
+from application import application as app
+application = app
 from application import application as app
